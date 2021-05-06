f7dbb741f5e5ba5f169d136ff9760eccf2e946b7
alecgn <alecgn@hotmail.com>
2021-05-06 20:04:21 -0300

    Initial commit

---
+++ src/CryptographyHelpers/CryptographyHelpers.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CryptographyHelpers.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine($"Hello from {nameof(CryptographyHelpers)}!");
+        }
+    }
+}
---
+++ src/CryptographyHelpers/CryptographyHelpers/ServiceLocator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace CryptographyHelpers
+{
+    internal sealed class ServiceLocator
+    {
+        internal static ServiceLocator Instance { get { return _lazyInstance.Value; } }
+        
+        private static readonly Lazy<ServiceLocator> _lazyInstance = new(() => new ServiceLocator());
+        private IDictionary<Type, Type> servicesType;
+        private IDictionary<Type, object> instantiatedServices;
+
+        private ServiceLocator()
+        {
+            this.servicesType = new ConcurrentDictionary<Type, Type>();
+            this.instantiatedServices = new ConcurrentDictionary<Type, object>();
+
+            this.BuildServiceTypesMap();
+        }
+
+        internal T GetService<T>()
+        {
+            if (this.instantiatedServices.ContainsKey(typeof(T)))
+            {
+                return (T)this.instantiatedServices[typeof(T)];
+            }
+            else
+            {
+                try
+                {
+                    ConstructorInfo constructor = servicesType[typeof(T)].GetConstructor(Array.Empty<Type>());
+                    Debug.Assert(constructor != null, "Cannot find a suitable constructor for " + typeof(T));
+                    T service = (T)constructor.Invoke(null);
+                    instantiatedServices.Add(typeof(T), service);
+
+                    return service;
+                }
+                catch (KeyNotFoundException)
+                {
+                    throw new ApplicationException("The requested service is not registered");
+                }
+            }
+        }
+
+        private void BuildServiceTypesMap()
+        {
+            //servicesType.Add(typeof(IService),
+            //    typeof(Service));
+        }
+    }
+}
